14deaa0000fa8e7c3b56177b2df82586105a9499
Harshit Jain <harshitjain0502@gmail.com>
2026-05-28 01:25:48 +0530

    Handle concurrent clients

--- app/main.go
+++ app/main.go
@@ -3,29 +3,46 @@ package main
 import (
 	"fmt"
 	"net"
-	"os"
 )
 
-func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+func handleClient(conn net.Conn) {
+	defer conn.Close()
 
 	for {
 		buf := make([]byte, 1024)
-		_, err := conn.Read(buf)
+
+		n, err := conn.Read(buf)
 		if err != nil {
-			fmt.Println("Error reading from connection: ", err.Error())
-			break
+			fmt.Println("Error reading from connection:", err.Error())
+			return
 		}
-		conn.Write([]byte("+PONG\r\n"))
+
+		fmt.Println("Received:", string(buf[:n]))
+
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			fmt.Println("Error writing to connection:", err.Error())
+			return
+		}
 	}
-	conn.Close()
+}
+
+func main() {
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+
+	fmt.Println("Server listening on port 6379")
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err.Error())
+			continue
+		}
+
+		go handleClient(conn)
+	}
 }
